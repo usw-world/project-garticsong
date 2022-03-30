4b93ebad55aac44a58acaecb711648d348b1a57b
usw-world <usw@gmail.com>
2022-03-30 17:54:24 +0900

    I resovling the icecandidate

--- src/MediaRelayer.svelte
+++ src/MediaRelayer.svelte
@@ -3,58 +3,73 @@
     import { io } from 'socket.io-client';
 
     const socket = io("http://10.30.5.129:3000/");
-
-
+    
     const ConnectAnother = () => {
         MakeCall();
     }
-    const configuration = {'iceServers': [{'urls':'stun:stun.l.google.com:19302'}]};
+    const configuration = {'iceServers': [
+        {
+            'urls':['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302']
+        },
+    ]};
+    const peerConnection = new RTCPeerConnection();
+
+    peerConnection.addEventListener("icecandidate", event => {
+        console.log("Ice event", event);
+        socket.emit({'newIceCandidate': event.candidate});
+    })
+    peerConnection.addEventListener('connectionstatechange', event => {
+        console.log(peerConnection.connectionState);
+        if (peerConnection.connectionState === 'connected') {
+            console.log(peerConnection);
+        }
+    });
+
+    console.log(peerConnection.getConfiguration(configuration));
+    // peerConnection.setConfiguration(configuration);
+    
+
     const MakeCall = async () => {
-        const peerConnection = new RTCPeerConnection(configuration);
-        console.log("usoock");
-        // peerConnection.addEventListener('connectionstatechange', event => {
-        //     console.log(peerConnection.connectionState);
-        //     if (peerConnection.connectionState === 'connected') {
-        //         console.log(peerConnection);
-        //     }
-        // });
-        // peerConnection.addEventListener("icecandidate", event => {
-        //     socket.emit({'newIceCandidate': event.candidate});
-        // })
+        socket.on("iceCandidate", async payload => {
+            try {
+                await peerConnection.addIceCandidate(payload);
+                console.log(peerConnection);
+            } catch (error) {
+                console.error('Error adding received ice candidate', e);
+            }
+        })
+        // console.log(peerConnection.getConfiguration(configuration));
+        // peerConnection.setConfiguration(configuration);
+        
         socket.on("answer", async payload => {
             const remoteDesc = new RTCSessionDescription(payload);
             await peerConnection.setRemoteDescription(remoteDesc);
-            console.log(peerConnection);
+            console.log("Receive answer.");
+            console.log(peerConnection.getConfiguration(configuration));
         });
         const offer = await peerConnection.createOffer();
         await peerConnection.setLocalDescription(offer);
         socket.emit("offer", {"offer": offer});
     }
 
-    (async () => {
-        const peerConnection = new RTCPeerConnection(configuration);
-        // peerConnection.addEventListener('connectionstatechange', event => {
-        //     console.log(peerConnection.connectionState);
-        //     if (peerConnection.connectionState === 'connected') {
-        //         console.log(peerConnection);
-        //     }
-        // });
+    const Serve = async () => {
         socket.on("offer", async payload => {
             peerConnection.setRemoteDescription(new RTCSessionDescription(payload));
             const answer = await peerConnection.createAnswer();
             await peerConnection.setLocalDescription(answer);
-            console.log(payload);
+            console.log("I receive offer");
             socket.emit("answer", answer);
+        });
+        socket.on("iceCandidate", async payload => {
+            try {
+                await peerConnection.addIceCandidate(payload);
+                console.log(peerConnection);
+            } catch (error) {
+                console.error('Error adding received ice candidate', e);
+            }
         })
-        // socket.on("iceCandidate", async payload => {
-        //     try {
-        //         await peerConnection.addIceCandidate(payload);
-        //         console.log(peerConnection);
-        //     } catch (error) {
-        //         console.error('Error adding received ice candidate', e);
-        //     }
-        // })
-    })();
+        console.log("Get ready to serve");
+    };
     /* << Socket io test area */
 </script>
 
@@ -63,9 +78,9 @@
         <div class="userinfo-name"></div>
         <div class="userinfo-headers"></div>
         <div class="userinfo-body"></div>
-        <button class="do-link" on:click="{ConnectAnother}">LINK!!</button>
+        <button class="do-link" on:click="{ConnectAnother}">LINK</button>
     </div>
     <div class="serveman">
-        <button>Serve!</button>
+        <button class="do-serve" on:click="{Serve}">SERVE</button>
     </div>
 </main>
