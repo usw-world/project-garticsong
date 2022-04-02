e92fbc59de6cf5bcf93c78dbbbd1e8d579f652b4
usoock13 <usoock13@gmail.com>
2022-04-02 20:54:34 +0900

    commit for test!

--- src/MediaRelayer.svelte
+++ src/MediaRelayer.svelte
@@ -2,29 +2,15 @@
     /* Socket io test area >> */
     import { io } from 'socket.io-client';
 
-    // const socket = io("https://garticsong.herokuapp.com/");
+    const socket = io("https://garticsong.herokuapp.com/");
     // const socket = io("http://10.30.5.129:2023");
-    const socket = io("http://192.168.219.101:2023");
-    
-    const ConnectAnother = () => {
-        MakeCall();
-    }
+    // const socket = io("http://192.168.219.101:2023");
     const configuration = {'iceServers': [
         {
             'urls':['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302']
         },
     ]};
     const peerConnection = new RTCPeerConnection(configuration);
-    
-    peerConnection.addEventListener("icecandidate", event => {
-        socket.emit({'newIceCandidate': event.candidate});
-    })
-    peerConnection.addEventListener('connectionstatechange', event => {
-        console.log(peerConnection.connectionState);
-        if (peerConnection.connectionState === 'connected') {
-            console.log(peerConnection);
-        }
-    });
 
     const GetUserMedia = async () => {
         try {
@@ -41,9 +27,12 @@
     }
     let OnSubmitMessage = () => {
         const channel = peerConnection.createDataChannel("usoock");
-        channel.addEventListener("open", event => {
+        peerConnection.ondatachannel = (event) => {
+            console.log("your fault!");
+        };
+        channel.onopen = (event) => {
             channel.send("Dragon Usoock!");
-        })
+        };
         console.log(channel);
     };
     
@@ -66,7 +55,7 @@
     }
 
     const MakeServe = async () => {
-        peerConnection.addEventListener("datachannel", event => {
+        peerConnection.ondatachannel = (event) => {
             console.log("I can Fly!");
             let channel = event.channel;
             channel.onopen = (event) => {
@@ -75,7 +64,7 @@
             channel.onmessage = (event) => {
                 console.log(event.data);
             }
-        })
+        }
             
         socket.on("offer", async payload => {
             peerConnection.setRemoteDescription(new RTCSessionDescription(payload));
@@ -84,14 +73,6 @@
             console.log("I receive offer");
             socket.emit("answer", answer);
         });
-        socket.on("iceCandidate", async payload => {
-            try {
-                await peerConnection.addIceCandidate(payload);
-                console.log(peerConnection);
-            } catch (error) {
-                console.error('Error adding received ice candidate', e);
-            }
-        })
         const audioElmt = document.querySelector("audio#localaudio");
         peerConnection.addEventListener("track", async(event) => {
             const[ remoteStream ] = event.streams;
@@ -107,7 +88,7 @@
         <div class="userinfo-name"></div>
         <div class="userinfo-headers"></div>
         <div class="userinfo-body"></div>
-        <button class="do-link" on:click="{ConnectAnother}">LINK</button>
+        <button class="do-link" on:click="{MakeCall}">LINK</button>
     </div>
     <div class="serveman">
         <button class="do-serve" on:click="{MakeServe}">SERVE</button>
